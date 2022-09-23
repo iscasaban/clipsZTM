1d9e916c7ae1e6b7db5697d0b5dc354ffba77815
Inma Sánchez Casabán <inma.sanchezcasaban@maibornwolff.de>
2022-09-23 10:21:20 +0200

    add error messages to login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +7,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
   credentials = {
     email: '',
     password: '',
   };
-  login() {}
+
+  showAlert = false;
+  alertMsg = 'Please wait! We are logging you in! ';
+  alertColor = 'blue';
+  inSubmission = false;
+
+  constructor(private auth: AngularFireAuth) {}
+
+  ngOnInit(): void {}
+
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! We are logging you in! ';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (error) {
+      this.inSubmission = false;
+      this.alertMsg = 'Something bad happened :( Please try again later';
+      this.alertColor = 'red';
+      return;
+    }
+    this.alertMsg = ` Awesome! You're in  `;
+    this.alertColor = 'green';
+  }
+
+  //Error in event handler: TypeError: Assignment to constant variable.
+
+  //async login() {
+  //   this.showAlert = true;
+  //   this.alertMsg = 'Please wait! We are logging you in! ';
+  //   this.alertColor = 'blue';
+  //   this.inSubmission = true;
+
+  //   try {
+  //     await this.auth.signInWithEmailAndPassword(
+  //       this.credentials.email,
+  //       this.credentials.password
+  //     );
+  //   } catch (error) {
+  //     this.inSubmission = false;
+  //     this.alertColor = 'red';
+  //     switch (error) {
+  //       case 'auth/invalid-email':
+  //         this.alertMsg = 'The email address is not valid. Please try again';
+  //         break;
+  //       case 'auth/user-disabled':
+  //         this.alertMsg = 'This user has been disabled';
+  //         break;
+  //       case 'auth/user-not-found':
+  //         this.alertMsg = 'There is no user corresponding to the given email';
+  //         break;
+  //       case 'auth/wrong-password':
+  //         this.alertMsg = 'Invalid password. Please try again';
+  //         break;
+  //       default:
+  //         this.alertMsg = 'Something bad happened :( Please try again later';
+  //         break;
+  //     }
+  //     return;
+  //   }
+  //   this.alertMsg = ` Awesome! You're in  `;
+  //   this.alertColor = 'green';
+  // }
 }
