a220526fecbb50637a74e7b840d9dd6f37d4fc33
Inma Sánchez Casabán <inma.sanchezcasaban@maibornwolff.de>
2022-09-28 16:35:37 +0200

    add test to nav component

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 import { NavComponent } from './nav.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
+  const mockedAuthService = jasmine.createSpyObj(
+    'AuthServive',
+    ['createUser', 'logout'],
+    {
+      isAuthenticated$: of(true),
+    }
+  );
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
-    })
-    .compileComponents();
+      declarations: [NavComponent],
+      imports: [RouterTestingModule],
+      providers: [{ provide: AuthService, useValue: mockedAuthService }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
