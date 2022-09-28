8ed087f2c3ebecf2f504f8300f6a372cc4cbb16a
Inma Sánchez Casabán <inma.sanchezcasaban@maibornwolff.de>
2022-09-28 16:08:21 +0200

    test nested components

---
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TabsContainerComponent } from './tabs-container.component';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  template: `
+    <app-tabs-container>
+      <app-tab tabTitle="Tab 1">Tab test 1</app-tab>
+      <app-tab tabTitle="Tab 2">Tab test 2</app-tab>
+    </app-tabs-container>
+  `,
+})
+class TestHostComponent {}
+
+describe('TabsContainerComponent', () => {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TabsContainerComponent, TabComponent, TestHostComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
