8e491350ba2d622eda11b996bc57a1052435b596
Inma Sánchez Casabán <inma.sanchezcasaban@maibornwolff.de>
2022-09-28 10:58:04 +0200

    add query params to manage component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ClipComponent } from './clip/clip.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'clip/:id',
+    component: ClipComponent,
   },
 ];
 
